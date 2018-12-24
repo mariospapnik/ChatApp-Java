79e97370a0991eaed3270f1eaab5d96117872d2c
marios <testmail@msn.com>
2018-12-24 20:52:06 +0200

    Update "showExpBox" for multiline String

---
+++ src/ChatApp/db/ChatUsersDAO.java
@@ -0,0 +1,38 @@
+package ChatApp.db;
+
+import ChatApp.core.Chat;
+import ChatApp.core.User;
+
+import java.util.ArrayList;
+
+public class ChatUsersDAO  extends Database {
+
+    public static ChatUsersDAO chatUsersDao = null;
+
+    private ChatUsersDAO(){}
+
+    public static ChatUsersDAO getInstance() {
+        if (chatUsersDao == null)
+            chatUsersDao = new ChatUsersDAO();
+        return chatUsersDao;
+    }
+
+    public int insertChatUsers(Chat chat, ArrayList<User> usersToInsert){
+        int rowsInserted = 0;
+        String table = "chat_users";
+        ArrayList<String> fields = new ArrayList<>();
+        ArrayList<String[]> values = new ArrayList<>();
+
+        fields.add("chat_id");
+        fields.add("user_id");
+
+        for (User user: usersToInsert) {
+            String[] rowValues = {String.valueOf(chat.id), String.valueOf(user.getID())};
+            values.add(rowValues);
+        }
+
+        rowsInserted = genericInsert(table, fields,  values);
+        return rowsInserted;
+    }
+
+}
